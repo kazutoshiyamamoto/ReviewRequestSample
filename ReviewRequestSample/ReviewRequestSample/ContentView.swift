54862a27de42752ea43db3b222b9848b39cb4924
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-06-14 20:00:10 +0900

    初回コミット

---
+++ ReviewRequestSample/ReviewRequestSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ReviewRequestSample
+//
+//  Created by home on 2021/06/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
