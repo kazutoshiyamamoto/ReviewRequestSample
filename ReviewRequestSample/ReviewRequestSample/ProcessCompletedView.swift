2b96d1a3cf4ba913546dabbe0f4f6fcf0826c6d1
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-06-14 21:01:13 +0900

    レビュー依頼の表示テスト用のViewを追加

---
+++ ReviewRequestSample/ReviewRequestSample/ProcessCompletedView.swift
@@ -0,0 +1,20 @@
+//
+//  ProcessCompletedView.swift
+//  ReviewRequestSample
+//
+//  Created by home on 2021/06/14.
+//
+
+import SwiftUI
+
+struct ProcessCompletedView: View {
+    var body: some View {
+        Text("Process Completed")
+    }
+}
+
+struct ProcessCompletedView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProcessCompletedView()
+    }
+}
