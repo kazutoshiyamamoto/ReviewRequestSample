255bc34beb5ec98ccac387fe79d617f47f997194
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-06-24 23:14:44 +0900

    UserDefaultsのキー名を扱うプロパティを追加

---
+++ ReviewRequestSample/ReviewRequestSample/UserDefaultsKeys.swift
@@ -0,0 +1,12 @@
+//
+//  UserDefaultsKeys.swift
+//  ReviewRequestSample
+//
+//  Created by home on 2021/06/23.
+//
+
+class UserDefaultsKeys {
+    class var processCompletedCountKey: String {
+        return "processCompletedCount"
+    }
+}
