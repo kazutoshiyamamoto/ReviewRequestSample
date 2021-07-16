f22b2ab5bc9e41a3356ac9ef8bbf3b74635ec05e
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-07-16 20:01:06 +0900

    一旦使用しない実装をコメントアウト

--- ReviewRequestSample/ReviewRequestSample/UserDefaultsKeys.swift
+++ ReviewRequestSample/ReviewRequestSample/UserDefaultsKeys.swift
@@ -1,12 +1,28 @@
+////
+////  UserDefaultsKeys.swift
+////  ReviewRequestSample
+////
+////  Created by home on 2021/06/23.
+////
 //
-//  UserDefaultsKeys.swift
-//  ReviewRequestSample
+//class UserDefaultsKeys {
+//    class var isStoreReviewHelperConfigured: String {
+//        return "isStoreReviewHelperConfigured"
+//    }
+//    
+//    class var isReviewRequestCandidate: String {
+//        return "isReviewRequestCandidate"
+//    }
+//    
+//    class var appOpenCount: String {
+//        return "appOpenCount"
+//    }
+//    
+//    class var processCompletedCount: String {
+//        return "processCompletedCount"
+//    }
 //
-//  Created by home on 2021/06/23.
-//
-
-class UserDefaultsKeys {
-    class var processCompletedCountKey: String {
-        return "processCompletedCount"
-    }
-}
+//    class var reviewRequestDate: String {
+//        return "reviewRequestDate"
+//    }
+//}
