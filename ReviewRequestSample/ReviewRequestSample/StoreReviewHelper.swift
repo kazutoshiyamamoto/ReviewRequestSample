8658d77ba8c53d6935492bef75917d50f70763e5
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-07-16 20:05:20 +0900

    レビュー依頼で使用するUserDefaultsKeyの一覧を定義

--- ReviewRequestSample/ReviewRequestSample/StoreReviewHelper.swift
+++ ReviewRequestSample/ReviewRequestSample/StoreReviewHelper.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+struct StoreReviewHelper {
+    // StoreReviewHelperで使用するUserDefaultsKeyの一覧
+    enum UserDefaultsKeys {
+        // 初回設定の完了状況を管理するキー
+        static let isStoreReviewHelperConfigured = "isStoreReviewHelperConfigured"
+        // レビュー依頼対象か管理するキー
+        static let isReviewRequestCandidate = "isReviewRequestCandidate"
+        // アプリを開いた回数を管理するキー
+        static let appOpenCount = "appOpenCount"
+        // 完了画面を表示した回数を管理するキー
+        static let processCompletedCount = "processCompletedCount"
+        // 最後にレビュー依頼を行った日時を管理するキー
+        static let lastReviewRequestDate = "lastReviewRequestDate"
+    }
+    
+}
