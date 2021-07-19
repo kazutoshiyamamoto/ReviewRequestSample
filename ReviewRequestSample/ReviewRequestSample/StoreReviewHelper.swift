cef353fd2e59d6261af2be75510a85d7f738b8a2
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-07-19 21:09:23 +0900

    UserDefaultsKeysのキー名を修正

--- ReviewRequestSample/ReviewRequestSample/StoreReviewHelper.swift
+++ ReviewRequestSample/ReviewRequestSample/StoreReviewHelper.swift
@@ -11,9 +11,9 @@ struct StoreReviewHelper {
     // StoreReviewHelperで使用するUserDefaultsKeyの一覧
     enum UserDefaultsKeys {
         // 初回設定の完了状況を管理するキー
-        static let StoreReviewHelperConfigured = "StoreReviewHelperConfigured"
+        static let StoreReviewHelperConfigured = "storeReviewHelperConfigured"
         // レビュー依頼対象か管理するキー
-        static let ReviewRequestCandidate = "ReviewRequestCandidate"
+        static let ReviewRequestCandidate = "reviewRequestCandidate"
         // アプリを開いた回数を管理するキー
         static let appOpenCount = "appOpenCount"
         // 完了画面を表示した回数を管理するキー
