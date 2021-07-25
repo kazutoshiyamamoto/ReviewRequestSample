fcfa6077aa9da4b6f19260e6b2f67d6fdecf91f4
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-07-25 20:44:56 +0900

    UserDefaultsKeysの命名を変更

--- ReviewRequestSample/ReviewRequestSample/StoreReviewHelper.swift
+++ ReviewRequestSample/ReviewRequestSample/StoreReviewHelper.swift
@@ -12,9 +12,9 @@ struct StoreReviewHelper {
     // StoreReviewHelperで使用するUserDefaultsKeyの一覧
     enum UserDefaultsKeys {
         // 初回設定の完了状況を管理するキー
-        static let StoreReviewHelperConfigured = "storeReviewHelperConfigured"
-        // レビュー依頼対象か管理するキー
-        static let ReviewRequestCandidate = "reviewRequestCandidate"
+        static let isConfigured = "isConfigured"
+        // レビュー依頼対象者か管理するキー
+        static let isRequestCandidate = "isRequestCandidate"
         // アプリを開いた回数を管理するキー
         static let appOpenCount = "appOpenCount"
         // 完了画面を表示した回数を管理するキー
@@ -25,38 +25,46 @@ struct StoreReviewHelper {
     
     // レビュー依頼候補者に設定
     static func configure() {
-        UserDefaults.standard.set(true, forKey: UserDefaultsKeys.ReviewRequestCandidate)
-        UserDefaults.standard.set(true, forKey: UserDefaultsKeys.StoreReviewHelperConfigured)
+        UserDefaults.standard.set(true, forKey: UserDefaultsKeys.isRequestCandidate)
         print("レビュー候補者に設定")
     }
     
-    // TODO:increment処理の共通化（UserDefaultsを拡張する？やり方調べる）
-    // アプリを開いた回数を1追加する
-    static func incrementAppOpenCount() {
-        var appOpenCount = UserDefaults.standard.integer(forKey: UserDefaultsKeys.appOpenCount)
-        appOpenCount += 1
-        UserDefaults.standard.set(appOpenCount, forKey: UserDefaultsKeys.appOpenCount)
-        print("アプリを開いた回数:\(appOpenCount)回")
+    // アプリを開いた回数を保存する
+    static func saveAppOpenCount() {
+        let appOpenCount = UserDefaults.standard.integer(forKey: UserDefaultsKeys.appOpenCount)
+        let newAppOpenCount = appOpenCount + 1
+        UserDefaults.standard.set(newAppOpenCount, forKey: UserDefaultsKeys.appOpenCount)
+        print("アプリを開いた回数:\(newAppOpenCount)回")
     }
     
-    // 完了画面を表示した回数を1追加する
-    static func incrementProcessCompletedCount() {
-        var processCompletedCount = UserDefaults.standard.integer(forKey: UserDefaultsKeys.processCompletedCount)
-        processCompletedCount += 1
-        UserDefaults.standard.set(processCompletedCount, forKey: UserDefaultsKeys.processCompletedCount)
-        print("完了画面を表示した回数:\(processCompletedCount)回")
+    // 完了画面を表示した回数を保存する
+    static func saveProcessCompletedCount() {
+        let processCompletedCount = UserDefaults.standard.integer(forKey: UserDefaultsKeys.processCompletedCount)
+        let newProcessCompletedCount = processCompletedCount + 1
+        UserDefaults.standard.set(newProcessCompletedCount, forKey: UserDefaultsKeys.processCompletedCount)
+        print("完了画面を表示した回数:\(newProcessCompletedCount)回")
     }
     
     // 最後にレビュー依頼した日付を保存する
-    static func storeLastReviewRequestDate() {
+    static func saveLastReviewRequestDate() {
         let today = Date()
         UserDefaults.standard.set(today, forKey: UserDefaultsKeys.lastReviewRequestDate)
-        print("最後にレビュー依頼をした日付:\(String(describing: UserDefaults.standard.object(forKey: UserDefaultsKeys.lastReviewRequestDate)!))")
+        print("最後にレビュー依頼をした日付:\(today))")
     }
     
-    // 前回のレビュー依頼日から1ヶ月が経過しているかどうかを確認
-    // レビュー依頼したことがない場合は、2回以上アプリを開いていて、完了画面を3回以上表示しているかどうかを確認
-    static func shouldReviewRequest() -> Bool {
+    // レビュー依頼候補から外す
+    // 評価1,2をつけた場合に呼ばれる
+    static func removeFromCandidate() {
+        UserDefaults.standard.set(false, forKey: UserDefaultsKeys.isRequestCandidate)
+        
+        // レビュー依頼画面表示の判定に使用していた値を破棄
+        UserDefaults.standard.removeObject(forKey: UserDefaultsKeys.appOpenCount)
+        UserDefaults.standard.removeObject(forKey: UserDefaultsKeys.processCompletedCount)
+        UserDefaults.standard.removeObject(forKey: UserDefaultsKeys.lastReviewRequestDate)
+        
+        print("レビュー依頼候補から外した\n起動回数:\(String(describing: UserDefaults.standard.integer(forKey: UserDefaultsKeys.appOpenCount)))回\n完了画面を表示した回数:\(String(describing: UserDefaults.standard.integer(forKey: UserDefaultsKeys.processCompletedCount)))回\n最後にレビュー依頼をした日付:\(String(describing: UserDefaults.standard.object(forKey: UserDefaultsKeys.lastReviewRequestDate)))")
+    }
+    
         if let lastReviewRequestDate = UserDefaults.standard.object(forKey: StoreReviewHelper.UserDefaultsKeys.lastReviewRequestDate) as? Date {
             var calendar = Calendar.current
             calendar.locale = .current
