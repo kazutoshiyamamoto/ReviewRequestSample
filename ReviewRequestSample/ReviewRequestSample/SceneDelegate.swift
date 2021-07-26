8725ca1d1d31cf43306fe6d5be2311657d34375e
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-07-26 23:05:53 +0900

    アプリを開いた回数を保存する

--- ReviewRequestSample/ReviewRequestSample/SceneDelegate.swift
+++ ReviewRequestSample/ReviewRequestSample/SceneDelegate.swift
@@ -52,6 +52,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
         
+        StoreReviewHelper.shared.updateAppOpenCount()
     }
     
     func sceneDidEnterBackground(_ scene: UIScene) {
