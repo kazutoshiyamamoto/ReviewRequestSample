f0d5265f2d77ea1fae680b1513436b8b9159ced8
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-07-26 22:58:59 +0900

    レビュー依頼の候補者に関する列挙型を追加

--- ReviewRequestSample/ReviewRequestSample/StoreReviewHelper.swift
+++ ReviewRequestSample/ReviewRequestSample/StoreReviewHelper.swift
@@ -2,10 +2,17 @@
 //  StoreReviewHelper.swift
 //  ReviewRequestSample
 //
+//  Created by home on 2021/07/25.
 //
 
 import Foundation
 import StoreKit
+
+enum ReviewCandidateState: String {
+    case candidate = "candidate" // アプリレビュー依頼候補者
+    case notCandidate = "notCandidate" // アプリレビュー依頼対象外
+    case notConfigured = "" // 初回起動時（CandidateState未設定）
+}
 
     
     
