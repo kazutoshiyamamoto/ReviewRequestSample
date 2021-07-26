4ac806b51eb150c46c8964257482ce5642a3d7d3
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-07-26 22:50:24 +0900

    レビュー依頼の判定に使用するデータを操作するための処理をプロトコルに定義

--- ReviewRequestSample/ReviewRequestSample/UserDefaultsDataStore.swift
+++ ReviewRequestSample/ReviewRequestSample/UserDefaultsDataStore.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+protocol DataStoreProtocol {
+    func fetchCandidateState(completion: (ReviewCandidateState) -> Void)
+    func fetchAppOpenedCount() -> Int
+    func fetchProcessCompletedCount() -> Int
+    func fetchLastReviewRequestDate() -> Any?
+    
+    func saveCandidateState(state: ReviewCandidateState)
+    func saveAppOpenedCount(count: Int)
+    func saveProcessCompletedCount(count: Int)
+    func saveLastReviewRequestDate(date: Date)
+    
+    func removeAppOpenedCount()
+    func removeProcessCompletedCount()
+    func removeLastReviewRequestDate()
+}
+
