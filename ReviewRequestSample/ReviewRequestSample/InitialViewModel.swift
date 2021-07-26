cd0af8bb4f14b79267c6ed22ac6c98bc6f59f195
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-07-26 23:08:07 +0900

    初期表示画面用のViewModelを追加

---
+++ ReviewRequestSample/ReviewRequestSample/InitialViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  InitialViewModel.swift
+//  ReviewRequestSample
+//
+//  Created by home on 2021/07/26.
+//
+
+import Foundation
+
+class InitialViewModel: ObservableObject {
+    @Published var isReviewRequestable = false
+    
+    func canRequestReview() {
+        StoreReviewHelper.shared.canRequestReview() {
+            isReviewRequestable = $0
+        }
+    }
+}
