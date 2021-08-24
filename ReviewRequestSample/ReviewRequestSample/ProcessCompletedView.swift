b3a1c84651dedb8b12a77e6052873a09ea364635
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-08-24 20:36:29 +0900

    送信完了表示のアニメーションを変更

--- ReviewRequestSample/ReviewRequestSample/ProcessCompletedView.swift
+++ ReviewRequestSample/ReviewRequestSample/ProcessCompletedView.swift
@@ -17,80 +17,82 @@ struct ProcessCompletedView: View {
     @State private var isPresented = false
     
     var body: some View {
-        ZStack {
-            VStack {
-                Spacer()
-                
-                Text("Process Completed")
-                    .font(.system(size: 30))
-                    .padding()
-                
-                HStack(spacing: 25) {
-                    ForEach(1 ..< maximumRating + 1) { ratingNumber in
-                        VStack {
-                            Image(systemName: ratingNumber > selectedRating ? "star" : "star.fill")
-                                .foregroundColor(Color("ratingIconColor"))
-                                .onTapGesture {
-                                    selectedRating = ratingNumber
-                                    
-                                    // 低評価を選択した場合はアプリのストアレビュー依頼対象から外す
-                                    if ratingNumber <= 2 {
-                                        StoreReviewHelper.shared.removeFromCandidate()
-                                    }
-                                    
-                                    // ポップアップ表示（非表示は自動で行う）
-                                    withAnimation(.easeIn(duration: 0.2)) {
-                                        isPresented = true
-                                    }
-                                    DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-                                        withAnimation(.easeOut(duration: 0.1)) {
-                                            isPresented = false
+        GeometryReader { geometry in
+            ZStack {
+                VStack {
+                    Text("Process Completed")
+                        .font(.system(size: 30))
+                        .padding()
+                    
+                    HStack(spacing: 25) {
+                        ForEach(1 ..< maximumRating + 1) { ratingNumber in
+                            VStack {
+                                Image(systemName: ratingNumber > selectedRating ? "star" : "star.fill")
+                                    .foregroundColor(Color("ratingIconColor"))
+                                    .onTapGesture {
+                                        selectedRating = ratingNumber
+                                        
+                                        // 低評価を選択した場合はアプリのストアレビュー依頼対象から外す
+                                        if ratingNumber <= 2 {
+                                            StoreReviewHelper.shared.removeFromCandidate()
+                                        }
+                                        
+                                        // ポップアップ表示（非表示は自動で行う）
+                                        withAnimation(.easeOut(duration: 1.0)) {
+                                            isPresented = true
+                                        }
+                                        DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+                                            withAnimation(.easeIn(duration: 1.0)) {
+                                                isPresented = false
+                                            }
                                         }
                                     }
-                                }
-                                .font(.title2)
-                            
-                            Text(rankTexts[ratingNumber])
-                                .font(.subheadline)
+                                    .font(.title2)
+                                
+                                Text(rankTexts[ratingNumber])
+                                    .font(.subheadline)
+                            }
                         }
                     }
+                    .frame(width: 300)
+                    .padding(.bottom, 20)
+                    
+                    if let linkURL = URL(string: "https://apps.apple.com/app/idXXXXXXXXXX?action=write-review") {
+                        Link("Link", destination: linkURL)
+                            .font(.system(size: 18))
+                            .padding(.bottom, 20)
+                    }
+                    
+                    Button("Start Over") {
+                        presentationMode.wrappedValue.dismiss()
+                    }
+                    .font(.system(size: 18))
                 }
-                .frame(width: 300)
-                .padding(.bottom, 20)
                 
-                if let linkURL = URL(string: "https://apps.apple.com/app/idXXXXXXXXXX?action=write-review") {
-                    Link("Link", destination: linkURL)
-                        .font(.system(size: 18))
-                        .padding(.bottom, 20)
+                if isPresented {
+                    OverlayView(isPresented: $isPresented)
+                        .position(x: geometry.size.width / 2, y: -70)
                 }
-                
-                Button("Start Over") {
-                    presentationMode.wrappedValue.dismiss()
-                }
-                .font(.system(size: 18))
-                
-                Spacer()
             }
-            
-            if isPresented {
-                OverLayView(isPresented: $isPresented)
-            }
+            .frame(width: geometry.size.width, height: geometry.size.height)
+            .onAppear(perform: {
+                StoreReviewHelper.shared.updateProcessCompletedCount()
+            })
         }
-        .onAppear(perform: {
-            StoreReviewHelper.shared.updateProcessCompletedCount()
-        })
     }
 }
 
-struct OverLayView: View {
+struct OverlayView: View {
     @Binding var isPresented: Bool
     
     var body: some View {
         Text("Submitted!")
-            .frame(width: 200, height: 50)
-            .foregroundColor(.black)
-            .background(Color(red: 0.9, green: 0.9, blue: 0.9, opacity: 0.9))
-            .cornerRadius(10)
+            .font(.subheadline)
+            .frame(width: 220, height: 50)
+            .background(Color.white)
+            .cornerRadius(100)
+            .shadow(color: Color(red: 0.85, green: 0.85, blue: 0.85), radius: 20)
+            .transition(.move(edge: .top))
     }
 }
 
